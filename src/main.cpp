cd73203338442b33a6c19d9c35da8253294a3383
Michael Bosse <metiscus@gmail.com>
2016-03-02 16:32:43 -0500

    Testing SDL integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <cstdio>
 #include <cstdint>
+#include <SDL.h>
 
 int main(int argc, char** argv)
 {
     printf("Hello World!\n");
+
+   	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) 
+	{
+    	fprintf(stderr, "\nUnable to initialize SDL:  %s\n", SDL_GetError());
+        return 1;
+    }
+ 
     return 0;
 }
